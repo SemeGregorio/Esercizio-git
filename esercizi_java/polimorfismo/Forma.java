1c3c294eceecfeb617fbabed244b31328e4ab3ab
TGKristoffer_ <KristofferGithub@outlook.it>
2026-07-27 18:44:14 +0200

    Esercizio polimorfismo
    Esercizio polimorfismo completato

---
+++ esercizi_java/polimorfismo/Forma.java
@@ -0,0 +1,7 @@
+public class Forma {
+
+    //Metodo per il calcolo dell'area
+    public void calcolaArea() {
+        System.out.println("Area della forma non specificata");
+    }
+}
---
+++ esercizi_java/polimorfismo/Triangolo.java
@@ -0,0 +1,19 @@
+public class Triangolo extends Forma {
+
+    private double base;
+    private double altezza;
+
+    //Costruttore
+    public Triangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //Override del metodo calcolaArea()
+    //Ho sostituito super.calcolaArea con quello che leggete qui sotto
+    @Override
+    public void calcolaArea() {
+        double area = (base * altezza) / 2;
+        System.out.println("L'area del triangolo è: " + area);
+    }
+}
