ca0e355dab2cddaa0383057999f78c43515d997b
TGKristoffer_ <KristofferGithub@outlook.it>
2026-07-28 16:54:52 +0200

    Classi_astratte
    Esercizio sulle classi astratte, completato

---
+++ esercizi_java/classi_astratte/Forma.java
@@ -0,0 +1,5 @@
+public abstract class Forma {
+
+    //Metodo per il calcolo dell'area
+    public abstract void calcolaArea();
+}
---
+++ esercizi_java/classi_astratte/Triangolo.java
@@ -0,0 +1,18 @@
+public class Triangolo extends Forma {
+
+    private double base;
+    private double altezza;
+
+    //Costruttore
+    public Triangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //Override del metodo calcolaArea()
+    @Override
+    public void calcolaArea() {
+        double area = (base * altezza) / 2;
+        System.out.println("L'area del triangolo è: " + area);
+    }
+}
