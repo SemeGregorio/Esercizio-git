87602e243f9bfbb7eca84f497f6d651210220af8
TGKristoffer_ <KristofferGithub@outlook.it>
2026-07-29 17:55:37 +0200

    interface
    Esercizio sulle interfacce

---
+++ esercizi_java/interface/Forma.java
@@ -0,0 +1,5 @@
+public interface Forma {
+    //metodo per il calcolo del area
+    void calcolaArea();
+
+}
---
+++ esercizi_java/interface/Rettangolo.java
@@ -0,0 +1,33 @@
+public class Rettangolo implements Forma {
+
+    private double base;
+    private double altezza;
+
+    //Costruttore
+    public Rettangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //Override del metodo calcolaArea()
+    @Override
+    public void calcolaArea() {
+        double area = base * altezza;
+        System.out.println("L'area del rettangolo è: " + area);
+    }
+
+    public static void main(String[] args) {
+
+        //Crea un oggetto Rettangolo
+        Rettangolo rettangolo = new Rettangolo(5, 3);
+
+        //Crea un oggetto Triangolo
+        Triangolo triangolo = new Triangolo(5, 3);
+
+        //Stampa l'area del rettangolo
+        rettangolo.calcolaArea();
+
+        //Stampa l'area del triangolo
+        triangolo.calcolaArea();
+    }
+}
