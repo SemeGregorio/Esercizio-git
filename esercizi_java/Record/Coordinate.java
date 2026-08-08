457d5070e9a86a6f76b501ed7efc23cbd7ad2c47
TGKristoffer_ <KristofferGithub@outlook.it>
2026-08-08 16:15:24 +0200

    Record
    Esercizio record

---
+++ esercizi_java/Record/Coordinate.java
@@ -0,0 +1,12 @@
+public class Coordinate {
+
+    public static void main(String[] args) {
+
+        //Crea un Punto
+        Punto punto = new Punto(5, 3);
+
+        //Stampa le coordinate
+        System.out.println("X: " + punto.x());
+        System.out.println("Y: " + punto.y());
+    }
+}
