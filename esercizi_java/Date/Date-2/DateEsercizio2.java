9fa5bd5b2a822e31e35537dded7e7a80b6451789
TGKristoffer_ <KristofferGithub@outlook.it>
2026-08-10 18:01:55 +0200

    Secondo esercizio Date

---
+++ esercizi_java/Date/Date-2/DateEsercizio2.java
@@ -0,0 +1,19 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateEsercizio2 {
+
+    public static void main(String[] args) {
+
+        //Crea un oggetto OffsetDateTime dalla stringa
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        //Formatta la data nel formato richiesto
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALIAN);
+
+        //Stampa la data sulla console
+        System.out.println(date.format(formatter));
+    }
+}
